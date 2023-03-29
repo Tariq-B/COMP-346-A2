e8f0f7f9bb28aaa6c05549df6c891db32b6b5418
Tariq Benmouh <tbenmouh@gmail.com>
2023-03-28 20:26:55 -0400

    add file

--- Driver.java
+++ Driver.java
@@ -13,7 +13,7 @@ public class Driver {
 
         //print for later
                 try {
-        PrintStream testoutput = new PrintStream("Phase3ServerThreadsTest.txt");
+        PrintStream testoutput = new PrintStream("Phase3ServerThreadsTest1.txt");
         System.setOut(testoutput);
 
         } catch (FileNotFoundException e) {
